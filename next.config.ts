caad68bb29bf411a75b8e0317369fde1ea9ff9db
phareedahadamu <phareedahadamu@gmail.com>
2025-02-13 23:53:18 +0100

    Fixing config issues

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-// next.config.js
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   output: "export",
-};
-
-export default nextConfig;
-module.exports = {
   images: {
     domains: ["res.cloudinary.com"], // Add Cloudinary's domain here
   },
 };
+module.exports = nextConfig;
