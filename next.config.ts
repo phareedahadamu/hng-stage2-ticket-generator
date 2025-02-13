e566a6f3b562411358ba3f2f5b2f13e06c382215
phareedahadamu <phareedahadamu@gmail.com>
2025-02-13 23:46:17 +0100

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
 // next.config.js
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
+};
+
+export default nextConfig;
 module.exports = {
   images: {
     domains: ["res.cloudinary.com"], // Add Cloudinary's domain here
